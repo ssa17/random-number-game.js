b3ca7c34e3f3cee53c7056a6139d5e9ae8cc0b47
Syed Ahmed <syed.ahmed@ada.ac.uk>
2023-01-20 14:57:55 +0000

    step #4

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ let num = 0;
 function generateRandomNumber() {
   return Math.floor(Math.random());
 }
+
+function differenceFromAnswer(guess, answer) {
+  if(guess === answer) {
+    return "Correct";
+  }
+
+  if(guess < answer) {
+    return "Too Low";
+  } else {
+    return "Too High";
+  }
+}
